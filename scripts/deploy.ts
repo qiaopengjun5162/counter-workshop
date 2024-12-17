1ff9aaff22d984068a67819acc4b55b37099f901
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2024-12-17 20:13:12 +0800

    finished step 15

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -31,7 +31,7 @@ async function main() {
   const myCallData = new CallData(sierraCode.abi);
   const constructor = myCallData.compile("constructor", {
     initial_value: 100,
-    kill_switch:
+    kill_switch_address:
       "0x05f7151ea24624e12dde7e1307f9048073196644aa54d74a9c579a257214b542",
     initial_owner: process.env.DEPLOYER_ADDRESS ?? "",
   });
